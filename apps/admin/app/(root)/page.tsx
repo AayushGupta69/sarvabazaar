2c35d7cfbd06c0bbd318edf19fb59b72389d91dc
Aayush Gupta <aayushgupta69official@gmail.com>
2024-10-26 01:52:22 +0530

    Completed the form for store-modal.tsx using react-hook-form and zod

--- apps/admin/app/(root)/page.tsx
+++ apps/admin/app/(root)/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div>
       <p>Hello Admin Dashboard</p>
