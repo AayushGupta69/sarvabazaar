79130e590d505c46d974a63adbca6061e1353846
Aayush Gupta <aayushgupta69official@gmail.com>
2024-11-02 08:30:41 +0530

    Completed navbar with functionality to view all the store, create new one and switch between them

--- apps/admin/app/(dashboard)/[storeId]/layout.tsx
+++ apps/admin/app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@sarvabazaar/database/client";
 import { redirect } from "next/navigation";
@@ -28,7 +29,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be a Navbar</div>
+      <Navbar />
       {children}
     </>
   );
