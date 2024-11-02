a2a544d89fa32de3bdbf2e06bd0260186dc38970
Aayush Gupta <aayushgupta69official@gmail.com>
2024-11-02 07:01:49 +0530

    Updayed root layout.tsx to add afterSignOutUrl in ClerkProvider

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/">
       <html lang="en" className={inter.className} suppressHydrationWarning>
         <body>
           <ToasterProvider />
