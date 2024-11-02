944c3594405dd8e01cba8d949e9a957612ccd12f
Aayush Gupta <aayushgupta69official@gmail.com>
2024-11-02 18:05:22 +0530

    Completed the settings page with functionality to change the name of the store, delete the store and also show the api routes.

---
+++ apps/admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,39 @@
+import { SettingsForm } from "@/components/settings-form";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@sarvabazaar/database/client";
+import { redirect } from "next/navigation";
+
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initalData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
