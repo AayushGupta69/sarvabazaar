67299d16f4d1743f8542ef09a2425efc131a50c9
Aayush Gupta <aayushgupta69official@gmail.com>
2024-11-02 12:29:03 +0530

    Implemented api routes for updating and deleting store settings/preferences

---
+++ apps/admin/app/api/stores/[storeId]/route.ts
@@ -0,0 +1,82 @@
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@sarvabazaar/database/client";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      storeId: string;
+    };
+  }
+) {
+  try {
+    const { userId } = await auth();
+    const body = await req.json();
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store Id is required", { status: 400 });
+    }
+
+    const store = await prisma.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (e) {
+    console.error("[STORE_PATCH]", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      storeId: string;
+    };
+  }
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store Id is required", { status: 400 });
+    }
+
+    const store = await prisma.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (e) {
+    console.error("[STORE_DELETE]", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
