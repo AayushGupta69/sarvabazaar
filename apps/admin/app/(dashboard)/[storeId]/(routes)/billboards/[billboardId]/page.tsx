65409edf9b439899881387760062e5a5315e8ef9
Aayush Gupta <aayushgupta69official@gmail.com>
2024-11-03 02:59:44 +0530

    Completed the billboard page to show a single billboard

---
+++ apps/admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,23 @@
+import { BillboardForm } from "@/components/billboard-form";
+import prisma from "@sarvabazaar/database/client";
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard = await prisma.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initalData={billboard} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardPage;
