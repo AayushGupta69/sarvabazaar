0fdeb95ad3e7d59f491df00bf70bde3d61536a5b
Aayush Gupta <aayushgupta69official@gmail.com>
2024-11-16 07:11:57 +0530

    Reorganized routes

---
+++ apps/store/app/(routes)/page.tsx
@@ -0,0 +1,23 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import Container from "@/components/container";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const billboard = await getBillboard("21c1c247-bad1-446f-af65-2341bc485385");
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+}
